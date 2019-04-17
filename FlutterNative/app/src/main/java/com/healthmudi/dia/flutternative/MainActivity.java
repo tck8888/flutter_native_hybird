6030d060a46d441b676b749884a866d5e42aaebf
tck <tck@dra100.com>
2019-04-17 13:19:07 +0800

    添加描述

--- FlutterNative/app/src/main/java/com/healthmudi/dia/flutternative/MainActivity.java
+++ FlutterNative/app/src/main/java/com/healthmudi/dia/flutternative/MainActivity.java
@@ -2,12 +2,31 @@ package com.healthmudi.dia.flutternative;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import io.flutter.facade.Flutter;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLoadFlutterModule;
+    private FrameLayout flContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLoadFlutterModule = (Button) findViewById(R.id.btn_load_flutter_module);
+        flContainer = (FrameLayout) findViewById(R.id.fl_container);
+        btnLoadFlutterModule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fl_container, Flutter.createFragment(""))
+                        .commit();
+            }
+        });
+
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # flutter_native_hybird
+## 同一级目录创建原生项目与flutter项目
+
+## 原生项目的 settings.gradle 文件添加以下内容
+```
+setBinding(new Binding([gradle:this]))
+evaluate(new File(
+        settingsDir.parentFile,
+        'flutter_module/.android/include_flutter.groovy'
+))
+```
+
+## 原生项目的 build.gradle文件添加flutter_module依赖
+```
+implementation project(':flutter')
+```
+
+## 异常 Error: Invoke-customs are only supported starting with Android O (--min-api 26)
+```
+  compileOptions {
+        sourceCompatibility 1.8
+        targetCompatibility 1.8
+    }
+	minSdkVersion的版本大于flutter_module/.android/app/build.gradle的配置
+```
+
+
